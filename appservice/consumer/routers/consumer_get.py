bec50ae90362dc53880f52efe32484a610450474
vova1995 <vovapetryna1995@gmail.com>
2018-12-21 01:00:59 +0200

    added cassandra db condig

--- appservice/consumer/routers/consumer_get.py
+++ appservice/consumer/routers/consumer_get.py
@@ -2,6 +2,8 @@ from consumer import REDIS, APP, SESSION
 from sanic import response
 from consumer.models import Messages
 from sqlalchemy import func
+from consumer.database import DatabaseManager
+
 
 @APP.route("/consumer", methods=['GET'])
 async def consumer_get_offset(request):
@@ -28,4 +30,18 @@ async def consumer_count(request):
     result = session.query(func.count(Messages.id)).scalar()
     return response.json({
         'rows': result
+    })
+
+
+@APP.route("/consumer_rows_cassandra", methods=['GET'])
+async def consumer_count(request):
+    """
+
+    :param request:
+    :return:
+    """
+    rows = DatabaseManager.cassandra_query_select()
+
+    return response.json({
+        'rows': rows
     })
--- appservice/run.py
+++ appservice/run.py
@@ -1,6 +1,7 @@
 from consumer import APP
-from consumer.database import CreateTable
+from consumer.database import CreateTable, CreateCassandraTable
 
 if __name__ == "__main__":
     CreateTable()
+    CreateCassandraTable()
     APP.run(host="0.0.0.0", port=5000, debug=False)
