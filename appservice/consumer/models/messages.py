39f64543c9a37b52e883419d46a7a44023f27644
vova1995 <vovapetryna1995@gmail.com>
2018-12-20 12:29:09 +0200

    added count rows methid

--- appservice/consumer/models/messages.py
+++ appservice/consumer/models/messages.py
@@ -1,12 +1,14 @@
 from sqlalchemy import Column, String, INTEGER
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from consumer import ENGINE
 
 
 Base = declarative_base()
 
 class Messages(Base):
+    """
+    Table Messages
+    """
     __tablename__ = 'messages'
 
     id = Column('id', INTEGER, primary_key=True, autoincrement=True)
@@ -20,14 +22,3 @@ class Messages(Base):
     @classmethod
     def create_db(cls):
         Base.metadata.create_all(bind=ENGINE)
-
-# Session = sessionmaker(bind=engine)
-#
-# session = Session()
-#
-# message = Message()
-# message.topic = 'my_topic'
-# message.message = 'hello'
-# session.add(message)
-# session.commit()
-# session.close()
--- appservice/consumer/routers/consumer_get.py
+++ appservice/consumer/routers/consumer_get.py
@@ -1,9 +1,31 @@
-from consumer import REDIS, APP
+from consumer import REDIS, APP, SESSION
 from sanic import response
+from consumer.models import Messages
+from sqlalchemy import func
 
 @APP.route("/consumer", methods=['GET'])
 def consumer_get_offset(request):
+    """
+    Method that gets current offset from redis
+    :param request:
+    :return: offset
+    """
     offset = REDIS.get('kafka')
     return response.json({
         'offset': offset
+    })
+
+
+@APP.route("/consumer_rows", methods=['GET'])
+def consumer_count(request):
+    """
+
+    :param request:
+    :return:
+    """
+    session = SESSION()
+
+    result = session.query(func.count(Messages.id)).scalar()
+    return response.json({
+        'rows': result
     })
