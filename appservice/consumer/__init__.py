3702b534a63550853818807349d316786a570b2f
vova1995 <vovapetryna1995@gmail.com>
2018-12-21 17:20:09 +0200

    add docer for all containers

--- appservice/consumer/__init__.py
+++ appservice/consumer/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import redis
 from .config import Config
 from cassandra.cluster import Cluster
+import time
 
 
 
@@ -11,14 +12,21 @@ from cassandra.cluster import Cluster
 APP = Sanic()
 APP.config.from_object(Config)
 
-ENGINE = create_engine("postgresql://appservice:appservice@localhost:5432/appservice")
+ENGINE = create_engine("postgresql://appservice:appservice@postgres:5432/appservice")
 SESSION = sessionmaker(bind=ENGINE)
 
 REDIS = redis.Redis(host=APP.config['REDIS_URL'], port=APP.config['REDIS_PORT'], db=0)
 
-CLUSTER = Cluster(["localhost"])
+CLUSTER = Cluster(["cassandra"])
 
 KEY_SPACE = 'messages'
-CASSANDRA_SESSION = CLUSTER.connect()
+while True:
+    try:
+        CASSANDRA_SESSION = CLUSTER.connect()
+        break
+    except Exception as e:
+        print(e)
+        time.sleep(10)
 
+# CASSANDRA_SESSION = CLUSTER.connect()
 from .routers import (consumer_get)
--- appservice/run.py
+++ appservice/run.py
@@ -1,7 +1,16 @@
 from consumer import APP
 from consumer.database import CreateTable, CreateCassandraTable
+import time
 
 if __name__ == "__main__":
     CreateTable()
-    CreateCassandraTable()
+    while True:
+        try:
+            CreateCassandraTable()
+            break
+        except Exception as e:
+            print(e)
+            time.sleep(10)
+    # CreateCassandraTable()
+
     APP.run(host="0.0.0.0", port=5000, debug=False)
